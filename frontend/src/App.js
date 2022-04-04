b09a26edd38976b33aa7c338bfd88a50cafe080c
chineduCoded <chinedujohn17@gmail.com>
2022-04-04 07:11:05 +0100

    Refactor codes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Expense Tracker</h1>
+      <div className="container">
+        <h1>Expense Tracker</h1>
+      </div>
     </div>
   );
 }
