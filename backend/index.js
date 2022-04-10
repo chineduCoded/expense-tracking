358a3e1d0d286518035bb2747563c4f59b3d7796
chineduCoded <chinedujohn17@gmail.com>
2022-04-10 13:57:53 +0100

    Update on Procfile

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,9 @@ app.use(express.json())
 app.use(cors())
 
 // use routes
+app.use("/", (req, res) => {
+    res.send("Welcome to my expense Tracker")
+})
 app.use("/api/categories", require("./routes/categoryRoutes"))
 app.use("/api/transaction", require("./routes/transactionRoutes"))
 app.use("/api/labels", require("./routes/labelRoutes"))
